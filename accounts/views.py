9ea588a8e73ef259ec13b433b10a7bbdf4508d79
anilchoudhary <masteranilchoudhary@gmail.com>
2020-02-19 22:27:09 +0530

    created app accounts

---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST['password'] == request.POST['confirmpassword']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username already exist.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password'])
+                login(request, user)
+                return render(request, 'accounts/signup.html')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Password didn\'t match.'})
+    else:
+        return render(request, 'accounts/signup.html')
+
+
+def loginview(request):
+    if request.method == "POST":
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            login(request, user)
+        else:
+            return render(request, 'accounts/login.html', {'error':'The username and password didn\'t match'})
+    else:
+        return render(request, 'accounts/login.html')
